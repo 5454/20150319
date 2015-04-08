5ec981a4ebfe06446065d14ea8c6dcb96b3bd08e
ZEP <545439194@qq.com>
2015-04-08 10:43:27 +0800

    10.43

---
+++ Assets/AssetBundleBuilder/AssetBunderLoader.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetBunderLoader : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        StartCoroutine(LoadRes());
+    }
+
+    IEnumerator LoadRes() {
+        Debug.Log(Application.dataPath + "/AssetBundleBuilder/ABs/cubebundle");
+        WWW loader = WWW.LoadFromCacheOrDownload("file://" + Application.dataPath + "/AssetBundleBuilder/ABs/cubebundle", 0);
+        yield return loader;
+        Debug.Log(loader.assetBundle);
+        Instantiate(loader.assetBundle.LoadAsset("Cube1"));
+    }
+
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
---
+++ Assets/AssetBundleBuilder/Editor/AssetBunderCreater.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+
+public class AssetBunderCreater : MonoBehaviour {
+
+    [MenuItem("ZZZZZ/AssetBunderCreaterBySelected")]
+    public static void ABCreater() {
+        AssetBundleBuild[] buildMap = new AssetBundleBuild[1];
+
+        buildMap[0].assetBundleName = "CubeBundle";
+
+        List<string> willBuildList = new List<string>();
+
+        for (int i = 0; i < Selection.gameObjects.Length; i++) {
+            GameObject go = Selection.gameObjects[i];
+            string assetPath = AssetDatabase.GetAssetPath(go);
+            string[] dependList = AssetDatabase.GetDependencies(new string[] { assetPath });
+            for (int j = 0; j < dependList.Length; j++) {
+                willBuildList.Add(dependList[j]);
+                Debug.Log("===> "+dependList[j]);
+            }
+            //willBuildList.Add(assetPath);
+        }
+        buildMap[0].assetNames = willBuildList.ToArray();
+
+        foreach (string x in willBuildList) {
+            //Debug.Log(x);
+        }
+
+
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/AssetBundleBuilder/ABs", buildMap, BuildAssetBundleOptions.None);
+    }
+
+    [MenuItem("ZZZZZ/AssetBunderCreaterByAutomatic")]
+    public static void ABCreaterAutomatic() {
+        BuildPipeline.BuildAssetBundles(Application.dataPath + "/AssetBundleBuilder/ABAs");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
